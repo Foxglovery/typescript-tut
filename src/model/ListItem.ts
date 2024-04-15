ab591860d78262b776aecb9162ad8ecd0eb9d7a7
Foxglovery <foxglovery@gmail.com>
2024-04-15 16:42:46 -0500

    commit

--- src/model/ListItem.ts
+++ src/model/ListItem.ts
@@ -12,6 +12,7 @@ export default class ListItem implements Item {
         private _checked: boolean = false,
     ) {}
 
+    // Getters and Setters
     get id(): string {
         return this._id
     }
@@ -19,4 +20,20 @@ export default class ListItem implements Item {
     set id(id: string) {
         this._id = id
     }
+
+    get item(): string {
+        return this._item
+    }
+
+    set item(item: string) {
+        this._item = item
+    }
+
+    get checked(): boolean {
+        return this._checked
+    }
+    set checked(checked: boolean) {
+        this._checked = checked
+    }
+
 }
